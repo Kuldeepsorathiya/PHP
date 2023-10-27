5b9ad1f6b3727c0c04bb1865a214dbf08c47ee10
Kuldip Sorathiya <145132196+Kuldeepsorathiya@users.noreply.github.com>
2023-10-27 19:51:01 +0530

    Add files via upload

---
+++ Array_Script/array_12.php
@@ -0,0 +1,9 @@
+<?php
+//12. Create an associative array representing a person's details, including name, age, and email.
+
+$personal_detail = array(
+    "name"=>"kuldip",
+    "age"=>"22",
+    "email"=>"rahulgautam7r@gmail.com");
+print_r($personal_detail);
+?>
